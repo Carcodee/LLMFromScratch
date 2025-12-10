52053a2af4ab43abbadbaa1dd62feb372578cded
Carlos Matecki <carlosmatecki2@gmail.com>
2025-12-09 20:46:05 -0500

    Adding generate function to generate chain of text

--- Python/transformer.py
+++ Python/transformer.py
@@ -1,5 +1,7 @@
 #%%
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import pandas as pd
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
@@ -34,17 +36,17 @@ src = tokenizer.encode(final_string)
 print(src.ids)
 '''
 
-def idx_to_vocab(list_of_idxs):
+def decode(list_of_idxs):
     list_of_words = [vocab_dict_inv[x] for x in list_of_idxs]
     return list_of_words
 
-def vocab_to_idx(list_of_words):
+def encode(list_of_words):
     list_of_idxs = [vocab_dict[x] for x in list_of_words]
     return list_of_idxs
 
 
 #%%
-batch_size = 12
+batch_size = 6 
 batch_count = 4
 vocab_size = len(vocab_dict)
 emb_dim= 32
@@ -78,18 +80,46 @@ for inp, tar in zip(inputs, targets):
         n+= 1
 
 # %%
-emb_table_output = torch.nn.Embedding(num_embeddings=vocab_size,embedding_dim=emb_dim, padding_idx=0)
-logits_x = emb_table_output(inputs)
-tok_emb = torch.nn.Embedding(num_embeddings=vocab_size,embedding_dim=emb_dim, padding_idx=0)
-pos_emb = torch.nn.Embedding(num_embeddings=vocab_size,embedding_dim=emb_dim, padding_idx=0)
-B, T, C = logits_x.shape 
-x_tok= tok_emb(inputs)
-cols = torch.arange(0, batch_count, 1).unsqueeze(1)
-rows = torch.arange(0, batch_size, 1).unsqueeze(0)
-pos = cols + rows
-x_pos= pos_emb(pos)
+class Transformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.tok_emb = nn.Embedding(num_embeddings=vocab_size,embedding_dim=emb_dim, padding_idx=0)
+        self.pos_emb = nn.Embedding(num_embeddings=vocab_size,embedding_dim=emb_dim, padding_idx=0)
+        self.lm_head = nn.Linear(emb_dim, vocab_size)
+    def forward(self, input, target = None):
+        B, T = input.shape
+        emb_tok = self.tok_emb(input)
+        emb_pos = self.pos_emb(torch.arange(T)).unsqueeze(0)
+        x = emb_pos + emb_tok
+        logits = self.lm_head(x)
+        loss = None
+        if target is not None:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)    
+            target = target.view(B*T)
+            loss = F.cross_entropy(logits, target)
 
-x = x_tok + x_pos
+        return logits, loss 
+# %%
+
+cTransformer = Transformer()
+optimizer = torch.optim.Adam(cTransformer.parameters(), lr=1e-3)
+targets = targets.long()
+
+for epoch in range(200):
+    optimizer.zero_grad(set_to_none = True)
+    y, loss = cTransformer(inputs, targets)
+    print(loss)
+    loss.backward()
+    optimizer.step()
+
+input_chain = torch.tensor([[0]])
+next_tok, loss = cTransformer(input_chain)
+print(F.softmax(next_tok, dim = 2).max())
+
 
 
 # %%
+def generate(input, context_lenght):
+
+    pass
