0f3fcf45674f53f86f890fbc8d56c079c195992c
Carlos Miguel Matecki-Savino <cmatecki@my.centennialcollege.ca>
2026-01-04 02:43:47 -0500

    Add simpler mnist sample to test autoencoders, it works fine with simple samples

--- Python/diffusion.py
+++ Python/diffusion.py
@@ -10,21 +10,42 @@ from pathlib import Path
 
 
 # %%
-target_img_size = [50, 50]
+target_img_size = [28, 28]
 train_size = 0.9
 channels = 1
 flatten_size = target_img_size[0] *  target_img_size[1] * channels
-latent_space_dim = 24
-latent_space_size = latent_space_dim * latent_space_dim
+latent_space_dim = 8 
+latent_space_size =16 
 
 #torch.manual_seed(329846)
 device =  'cuda' if torch.cuda.is_available() else 'cpu'
 
 # %%
-images_path = './images_data/human'
-p = Path(images_path)
-img_tensor_uint8 = torch.stack([TF.resize(read_image(item.resolve()), target_img_size)
-                                           for item in p.iterdir()]).to(device=device)
+images_path = './images_data/mnist/trainingSet/0'
+paths = []
+all_images = []
+
+all_paths = Path(images_path)
+#for directory in all_paths.iterdir():
+#    paths.append(Path(directory))
+
+paths.append(Path('./images_data/mnist/trainingSet/0'))
+paths.append(Path('./images_data/mnist/trainingSet/1'))
+paths.append(Path('./images_data/mnist/trainingSet/2'))
+paths.append(Path('./images_data/mnist/trainingSet/3'))
+paths.append(Path('./images_data/mnist/trainingSet/4'))
+
+for p in paths:
+    for item in p.iterdir():
+        all_images.append(TF.resize(read_image(item.resolve()), target_img_size)[:3, :, :])
+
+
+img_tensor_uint8 = torch.stack(all_images).to(device=device)
+
+rand_indices = torch.randint(0, img_tensor_uint8.shape[0], (img_tensor_uint8.shape[0], ))
+img_tensor_uint8 = img_tensor_uint8[rand_indices, :, :, :]
+
+print(img_tensor_uint8.shape)
                                 
 
 # %%
@@ -32,25 +53,23 @@ imgs_tensor = img_tensor_uint8.float().to(device=device) / 255.0
 
 #H,W,C
 imgs_tensor = imgs_tensor.transpose(dim0=1, dim1=-1).transpose(dim0=1, dim1=2).to(device=device)
-imgs_tensor = imgs_tensor[:, :, :, 3 - channels:]
+if channels != 1:
+    imgs_tensor = imgs_tensor[:, :, :, 3 - channels:]
+
 print(imgs_tensor.shape)
 train_size_int = int(imgs_tensor.shape[0] * train_size)
 
 train_images_x = imgs_tensor[:train_size_int].clone().to(device=device)
 test_images_x = imgs_tensor[train_size_int:].clone().to(device=device)
 
-torch.manual_seed(123465)
-mu = 0
-std_train = 0.1
-std_test = 0.1
-random_nums_train = torch.randn_like(train_images_x).to(device=device)
-random_nums_test = torch.randn_like(test_images_x).to(device=device)
-
-train_images_x = train_images_x + random_nums_train * std_train
-test_images_x = test_images_x + random_nums_test * std_test
-
 train_images_y = imgs_tensor[:train_size_int].to(device=device)
 test_images_y = imgs_tensor[train_size_int:].to(device=device)
+
+
+torch.manual_seed(123465)
+mu = 0
+std_train = 0.5
+
 
 
 
@@ -59,6 +78,7 @@ def get_batch_random(src_x, src_y, batch_size, flatten = True, channels_first_di
     batches_to_pick = torch.randint(0, src_x.shape[0], (batch_size, )).to(device=device)
     batches_x = src_x[batches_to_pick].clone().to(device=device)
     batches_y = src_y[batches_to_pick].clone().to(device=device)
+    batches_x = batches_x + torch.randn_like(batches_x) * std_train
     if flatten == True:
         batches_x = batches_x.reshape(batch_size, -1).to(device=device)
         batches_y = batches_y.reshape(batch_size, -1).to(device=device)
@@ -72,10 +92,25 @@ def get_batches(src_x, src_y, batch_size = -1, flatten = True, channels_first_di
         batch_size = src_x.shape[0]
     batches_x = src_x[:batch_size].clone().to(device= device)
     batches_y = src_y[:batch_size].clone().to(device= device)
+    batches_x = batches_x + torch.randn_like(batches_x) * std_train
     if flatten == True:
         batches_x = batches_x.reshape(batch_size, -1).to(device= device)
         batches_y = batches_y.reshape(batch_size, -1).to(device= device)
-    print(batches_x.shape)
+    if channels_first_dim == True:
+        batches_x =torch.transpose(batches_x, dim0= -1, dim1=1) 
+    return batches_x, batches_y
+
+def get_batch_sliced(src_x, src_y, batch_size, batch_idx, flatten = True, channels_first_dim = True):
+    if batch_size == -1:
+        #pick all batches
+        batch_size = src_x.shape[0]
+    offset = batch_size * batch_idx 
+    batches_x = src_x[offset:offset + batch_size].clone().to(device= device)
+    batches_y = src_y[offset:offset + batch_size].clone().to(device= device)
+    batches_x = batches_x + torch.randn_like(batches_x) * std_train
+    if flatten == True:
+        batches_x = batches_x.reshape(batch_size, -1).to(device= device)
+        batches_y = batches_y.reshape(batch_size, -1).to(device= device)
     if channels_first_dim == True:
         batches_x =torch.transpose(batches_x, dim0= -1, dim1=1) 
     return batches_x, batches_y
@@ -93,48 +128,38 @@ def get_random_latent_img(latent_size):
 class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
-#        self.model = torch.nn.Sequential(
-#            torch.nn.Linear(flatten_size, latent_space_size * 2),
-#            torch.nn.Conv2d(1, 4, 3, 1),
-#            torch.nn.Linear(latent_space_size * 2, latent_space_size),
-#            torch.nn.ReLU(),
-#       )
-        self.conv_1 = torch.nn.Conv2d(1, 4, 4, 1)
-        self.conv_2 = torch.nn.Conv2d(4, 8, 4, 1)
-        self.conv_3 = torch.nn.Conv2d(8, 8, 4, 1)
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(flatten_size, 512),
+            torch.nn.BatchNorm1d(512),
+            torch.nn.Dropout(0.2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 128),
+            torch.nn.BatchNorm1d(128),
+            torch.nn.Dropout(0.2),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(128, latent_space_size),
+            torch.nn.Sigmoid(),
+       )
     def forward(self, x):
-        x = F.relu(self.conv_1(x))
-        #4 * 47 * 47
-        x = F.max_pool2d(x, 2)
-        #4 * 23 * 23 
-        x = F.relu(self.conv_2(x))
-        #8 * 20 * 20 
-        x = F.max_pool2d(x, 2)
-        #8 * 10 * 10 
-        x = F.relu(self.conv_3(x))
-        #8 * 7 * 7 
-        x = F.max_pool2d(x, 2)
-        #8 * 3 * 3 
-        return (x)
+        return self.model(x)
 
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
-#        self.model = torch.nn.Sequential(
-#            torch.nn.Linear(latent_space_size, latent_space_size * 2),
-#            torch.nn.Linear(latent_space_size * 2, flatten_size),
-#        )
-        self.deconv_1 = torch.nn.ConvTranspose2d(8, 8, 2, 1)
-        self.deconv_2 = torch.nn.ConvTranspose2d(8, 4, 8, 1)
-        self.deconv_3 = torch.nn.ConvTranspose2d(4, 1, 32, 1)
-        self.fwd_1 = torch.nn.Linear(42 * 42, flatten_size)
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(latent_space_size, 128),
+            torch.nn.BatchNorm1d(128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 512),
+            torch.nn.BatchNorm1d(512),
+            torch.nn.Dropout(0.2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, flatten_size),
+            torch.nn.Dropout(0.2),
+            torch.nn.Sigmoid(),
+        )
     def forward(self, x):
-        x = self.deconv_1(x)
-        x = self.deconv_2(x)
-        x = self.deconv_3(x)
-        x = torch.flatten(x, 1)
-        x = self.fwd_1(x)
-        return x
+        return self.model(x)
 
 class AutoEncoder(nn.Module):
     def __init__(self, ):
@@ -174,20 +199,28 @@ for name, module in auto_encoder.named_modules():
 
 # %%
 #torch.seed()
+batch_size = 1000
+batch_count = int(float(train_images_x.shape[0]) / 1000.0)
+batch_count = 1
+print(batch_count)
+
+# %%
 torch.manual_seed(123465)
-for n in range(10000):
+for n in range(1000):
     optimizer.zero_grad(set_to_none=True)
-    batch_x, batch_y = get_batch_random(train_images_x, train_images_y, 80, False)
-    batch_x = batch_x.detach().clone().requires_grad_(True)
+    batch_x, batch_y = get_batch_random(train_images_x, train_images_y, batch_size)
+#        batch_x = batch_x.detach().clone().requires_grad_(True)
     x, loss = auto_encoder(batch_x, batch_y, 'none')
     x.retain_grad()
     loss.backward()
     optimizer.step()
     if n % 50 == 0:
         with torch.no_grad():
-            batch_x, batch_y = get_batch_random(test_images_x, test_images_y, 80, False)
+            auto_encoder.eval()
+            batch_x, batch_y = get_batches(test_images_x, test_images_y)
             x, _ = auto_encoder(batch_x, batch_y, 'none')
-        print(f'epoch {n}: train loss: {loss}, validation: {_}')
+            print(f'epoch {n}: train loss: {loss}, validation: {_}')
+    auto_encoder.train(True)
     
 #%%
 dead_neurons = {}
@@ -207,10 +240,9 @@ plt.imshow(mask, cmap='gray')
 
 #%%
 with torch.no_grad():
+    auto_encoder.eval()
     torch.manual_seed(123465)
-    data_point_x, data_point_y = get_batches(train_images_x, train_images_y, batch_size=-1, flatten=False)
-    print(data_point_x.shape)
-    print(data_point_y.shape)
+    data_point_x, data_point_y = get_batches(test_images_x, test_images_y, batch_size=-1, flatten=True)
     y, _ = auto_encoder(data_point_x, None, 'none')
     y = y.cpu()
     x_in = get_img_from_flatten(data_point_x.cpu())
@@ -218,6 +250,7 @@ with torch.no_grad():
     y = get_img_from_flatten(y)
 #    plt.imshow(x_in[0], cmap='gray')
 #    plt.imshow(y_in[0], cmap='gray')
-    plt.imshow(y[0], cmap='gray')
+    plt.imshow(y[100], cmap='gray')
+    print(F.mse_loss(y, y_in))
 
 # %%
